d716678a6cc599c613088a492f747e0f38dcc12c
SSolstice <72791899+SSolstice@users.noreply.github.com>
2021-01-10 20:12:41 +0800

    Create wordy.c

---
+++ wordy.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#define N 100
+int main()
+{
+	bool b = false;
+	int n,i,len,j,k,h;
+	char ch[N],a[N][N];
+	gets(ch);
+	len = strlen(ch);
+	for(i = 0,j = 0,k = 0;i < len;i++){
+		if(ch[i] != ' '){
+			a[j][k] = ch[i];
+			k++;
+		}
+		else{
+			j++;k = 0;h = j;
+		}
+	}
+	for(j = 0;j <= h;j++){
+		if(!b && (a[j][0] >= '0' && a[j][0] <= '9')){
+		    n = atoi(a[j]);
+		    b = true;
+	    }
+	    else{
+	    	if(strcmp(a[j],"plus") == 0)
+			    n += atoi(a[j+1]);
+		    else if(strcmp(a[j],"minus") == 0)
+			    n -= atoi(a[j+1]);
+		    else if(strcmp(a[j],"multiplied") == 0 && strcmp(a[j+1],"by") == 0)
+		        n *= atoi(a[j+2]);
+		    else if(strcmp(a[j],"divided") == 0 && strcmp(a[j+1],"by") == 0)
+		        n /= atoi(a[j+2]);
+		}
+	}
+	printf("%d",n);
+	return 0;
+}
