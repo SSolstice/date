2f96452d168071dd3a9872f5781d19dd7fc0303c
SSolstice <72791899+SSolstice@users.noreply.github.com>
2021-01-10 20:18:28 +0800

    Update wordy.c

--- wordy.c
+++ wordy.c
@@ -7,7 +7,9 @@ int main()
 	bool b = false;
 	int n,i,len,j,k,h;
 	char ch[N],a[N][N];
+	printf("Please enter a sentence about a math problem: \n");
 	gets(ch);
+	printf("\n");
 	len = strlen(ch);
 	for(i = 0,j = 0,k = 0;i < len;i++){
 		if(ch[i] != ' '){
@@ -34,6 +36,6 @@ int main()
 		        n /= atoi(a[j+2]);
 		}
 	}
-	printf("%d",n);
+	printf("The answer is %d.\n",n);
 	return 0;
 }
